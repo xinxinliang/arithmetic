483f60df5ec91dc400499b5e4fa1c8ff8ebf32d3
xinxinliang <liangxinxin5102@163.com>
2021-03-19 09:23:35 +0800

    linux 打开文件 读取文件内容到缓存区

--- file/mycopy.c
+++ file/mycopy.c
@@ -1,37 +1,33 @@
 /*mycopy.c*/
 #include <stdio.h>
-#include <errno.h>
+#include <time.h>
+#include <unistd.h>
 #define N 64
 
 int main(int argc,char *argv[]) {
     int n;
     char buf[N];
-    FILE *fps,*fpd;
+    FILE *fps;
+    time_t t;
 
-    if (argc<3)
+    if (argc<2)
     {
-        printf("Usage:%s<src_file><dst_file>\n",argv[0]);
+        printf("Usage:%s<file>\n",argv[0]);
         return -1;
     }
 
-    if((fps = fopen(argv[1],"r")) == NULL)
+    if((fps = fopen(argv[1],"w")) == NULL)
     {
-        fprintf(stderr,"fail to fopen $s\n",argv[1],strerror(errno));
+        perror("fail to open");
         return -1;
     }
-
-    if((fpd = fopen(argv[2],"w"))==NULL)
+    
+    while (1)
     {
-        fprintf(stderr,"fail to open %s\n",argv[2],strerror(errno));
-        fclose(fps);
-        return  -1;
-    }
-
-    while ((n=fread(buf,1,N,fps)) >= 0)
-    {
-        fwrite(buf,1,N,fpd);
+        time(&t);
+        fprintf(fps,"%s\n",ctime(&t));
+        sleep(1);
     }
     fclose(fps);
-    fclose(fpd);
     return 0;
 }
